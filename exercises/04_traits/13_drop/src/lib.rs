5c06097a2c87ca50929d70e662685083215f12a5
Pete LeVasseur <plevasseur@gmail.com>
2026-09-18 10:50:37 +0200

    solve 04/13

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,28 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    armed: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        DropBomb { armed: true }
+    }
+
+    pub fn defuse(&mut self) {
+        self.armed = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.armed {
+            panic!("Dropped DropBomb while armed!");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
